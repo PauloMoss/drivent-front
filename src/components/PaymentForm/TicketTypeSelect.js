f89a4e73976052b60665b3c199864c9c58d676c5
Iuri Magnago <iurimp91@gmail.com>
2021-09-02 00:17:28 -0300

    feat: payment page ticket type select

--- src/components/PaymentForm/TicketTypeSelect.js
+++ src/components/PaymentForm/TicketTypeSelect.js
@@ -1,16 +1,63 @@
 import styled from "styled-components";
 import Typography from "@material-ui/core/Typography";
+import { useState } from "react";
 
 export default function TicketTypeSelect() {
+  const [ticketType, setTicketType] = useState();
+
+  function selectTicketType(type) {
+    setTicketType(type);
+    console.log(type);
+  }
+
   return (
     <>
       <StyledTypography variant="h6">Primeiro, escolha sua modalidade de ingresso</StyledTypography>
+      <OptionBoxContainer>
+        <OptionBox onClick={(e) => selectTicketType("Presencial")} active={ticketType === "Presencial" ? true : false}>
+          <Description>Presencial</Description>
+          <Price>R$ 250</Price>
+        </OptionBox>
+        <OptionBox onClick={(e) => selectTicketType("Online")} active={ticketType === "Online" ? true : false}>
+          <Description>Online</Description>
+          <Price>R$ 100</Price>
+        </OptionBox>
+      </OptionBoxContainer>
     </>
   );
 }
 
 const StyledTypography = styled(Typography)`
-  margin-bottom: 20px!important;
+  margin-bottom: 17px!important;
   color: #8E8E8E;
   margin-top: 37px!important;
+`;
+
+const OptionBoxContainer = styled.div`
+  display: flex;
+`;
+
+const OptionBox = styled.div`
+  width: 145px;
+  height: 145px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: ${props => props.active ? "none" : "1px solid #CECECE"};
+  border-radius: 20px;
+  margin-right: 24px;
+  cursor: pointer;
+  background-color: ${props => props.active ? "#FFEED2" : "transparent"};
+`;
+
+const Description = styled.span`
+  font-size: 16px;
+  color: #454545;
+  margin-bottom: 5px;
+`;
+
+const Price = styled.div`
+  font-size: 14px;
+  color: #898989;
 `;
--- src/components/PaymentForm/index.js
+++ src/components/PaymentForm/index.js
@@ -1,17 +1,47 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Typography from "@material-ui/core/Typography";
+
+import useApi from "../../hooks/useApi";
 
 import TicketTypeSelect from "./TicketTypeSelect";
 
 export default function PaymentForm() {
+  const { enrollment } = useApi();
+  const [isEnrolled, setIsEnrolled] = useState(false);
+          
+  useEffect(() => {
+    enrollment.getPersonalInformations().then(response => {
+      if (response.status !== 200) {
+        return;
+      }
+      setIsEnrolled(true);
+    });
+  }, []);
+
+  console.log(isEnrolled);
+  
   return (
     <>
       <StyledTypography variant="h4" color="initial">Ingresso e pagamento</StyledTypography>
-      <TicketTypeSelect />
+      {
+        isEnrolled
+          ? <TicketTypeSelect />
+          : <NoEnrollmentWarning variant="h6">
+              Você precisa completar sua inscrição antes<br/> de prosseguir pra escolha de ingresso
+          </NoEnrollmentWarning>
+      }
     </>
   );
 }
 
 const StyledTypography = styled(Typography)`
   margin-bottom: 20px!important;
+`;
+
+const NoEnrollmentWarning = styled(Typography)`
+  color: #8E8E8E;
+  text-align: center;
+  margin-top: 180px!important;
+  line-height: 23px!important;
 `;
