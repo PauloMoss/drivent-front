45e272479c67d39717df71f5192957fd1dea17a9
Iuri Magnago <iurimp91@gmail.com>
2021-09-02 18:01:05 -0300

    fix: sign in wrong password crash error

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -33,7 +33,7 @@ export default function SignIn() {
       /* eslint-disable-next-line no-console */
       console.error(error);
       
-      if (error.response) {
+      if (error.response?.data?.details) {
         for (const detail of error.response.data.details) {
           toast(detail);
         }
