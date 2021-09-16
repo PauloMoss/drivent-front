679c66a07e1623579c4656b32f75ca6f58283eaa
ptrkc <pcarneiro.dev@gmail.com>
2021-09-16 16:32:25 -0300

    feat: activities live update

--- src/components/ActivityForm/ActivitiesOptions/ActivitiesBoard.js
+++ src/components/ActivityForm/ActivitiesOptions/ActivitiesBoard.js
@@ -9,6 +9,7 @@ export default function ActivitiesBoard({ selectedDay, activities }) {
   const mappedEventLocations = activities?.map(a => a.auditorium);
   const eventDays = mappedEventDays?.filter((day, i) => mappedEventDays.indexOf(day) === i);
   const eventLocations = mappedEventLocations?.filter((location, i) => mappedEventLocations.indexOf(location) === i);
+  const orderedLocations = eventLocations.sort((a, b) => a.localeCompare(b));
   const [selectedDayActivities, setSelectedDayActivities] = useState({});
   useEffect(() => {
     const filteredActivities = {};
@@ -21,9 +22,9 @@ export default function ActivitiesBoard({ selectedDay, activities }) {
 
   return (
     <>
-      <Locations>{eventLocations.map(location => <span key={location + "title"}>{location}</span>)}</Locations>
-      <Board collumns={eventLocations.length}>
-        {eventLocations.map(location => {
+      <Locations>{orderedLocations.map(location => <span key={location + "title"}>{location}</span>)}</Locations>
+      <Board collumns={orderedLocations.length}>
+        {orderedLocations.map(location => {
           return <Collumn key={location + "collumn"}>
             {selectedDayActivities[location]?.map(activity => {
               return <ActivityCard key={activity.id} activityData={activity} />;
